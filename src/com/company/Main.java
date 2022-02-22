cb9f33b941eb7475ed7c5c32931c1fbed9b0b501
sergey-r88 <mcmouse88@icloud.com>
2022-02-22 23:36:45 +0200

    Improve class Main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,14 +12,15 @@ public class Main {
         MissedCallsList missedCallsList = new MissedCallsList();
         phoneBook.fillPhoneBook();
 
+        System.out.println("Печатается список пропущенных звонков ...");
         missedCallsList.addMissedCall(LocalDateTime.now(), "8 (962) 233-38-09");
-        Thread.sleep(10_000 + RAND.nextInt(50_000 - 10_000));
+        Thread.sleep(2_000 + RAND.nextInt(5_000 - 2_000));
         missedCallsList.addMissedCall(LocalDateTime.now(), "8 (909) 787-83-12");
-        Thread.sleep(10_000 + RAND.nextInt(50_000 - 10_000));
+        Thread.sleep(2_000 + RAND.nextInt(5_000 - 2_000));
         missedCallsList.addMissedCall(LocalDateTime.now(), "8 (911) 477-26-31");
-        Thread.sleep(10_000 + RAND.nextInt(50_000 - 10_000));
+        Thread.sleep(2_000 + RAND.nextInt(5_000 - 2_000));
         missedCallsList.addMissedCall(LocalDateTime.now(), "8 (999) 999-99-99");
-        Thread.sleep(10_000 + RAND.nextInt(50_000 - 10_000));
+        Thread.sleep(2_000 + RAND.nextInt(5_000 - 2_000));
         missedCallsList.addMissedCall(LocalDateTime.now(), "8 (988) 888-88-88");
         System.out.println(missedCallsList);
     }
